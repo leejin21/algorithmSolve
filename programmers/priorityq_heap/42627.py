d1abf97bccf8edac3fade7b100cfb557f2386a6f
Jin Lee <leejin21c@gmail.com>
2021-03-16 01:18:07 +0900

    프로그래머스: 디스크 컨트롤러 end

--- programmers/priorityq_heap/42627.py
+++ programmers/priorityq_heap/42627.py
@@ -32,63 +32,100 @@ last_end =
 
 
 * 여러 가지 경우 생각하기 *
-1. [0, 10], [2, 10], [9, 10], [15, 2] 의 경우에 [15, 2]를 
-
+1. [0, 10], [2, 10], [9, 10], [15, 2] 의 경우에 [15, 2]가 [2,10] 수행 이후에 수행하는 게 평균 작업 시간을 최소로 하는 방법이다. 그래서 changeValueInHeap을 도입해 줌.
+2. [[1, 9], [1, 4], [1, 5], [1, 7], [1, 3]]에서: [1,3] 부터 시작된다고 함. 이때 주의할 건, jobs가 순서대로 안 들어올 수도 있다는 거임. 차라리 처음에 jobs를 정렬하고 시작하는 게 답이었다.
 
 '''
 import heapq
+from collections import deque
 
-first_le = 0
-tot = 0; last_end = 0
+tot = 0; last_end = 0; temp = deque()
 
 def solution(jobs):
-    global first_le, tot, last_end
-    
-    i = 0; heap = []
+    global tot, last_end, temp
+    tot = 0; last_end = 0; i = 0; heap = []
+    jobs.sort()     # jobs가 순서대로 안 들어오기 때문에 jobs를 순서대로 정렬해주고 시작하기
     while (i<len(jobs)):
-        print("job: ", jobs[i])
         if jobs[i][0] >= last_end:
             # 하드디스크가 작업을 수행하고 있지 않는 경우
             while(len(heap) != 0):
                 popJob(heap, jobs)
                 if jobs[i][0] < last_end: break
-            
             if jobs[i][0] >= last_end:
                 # 위 while문에 의해 갱신된 last_end에 대해서 job의 수행 시작 시간이 더 뒤에 있는지
                 # 즉, 하드 디스크가 놀고 있는지
                 tot += jobs[i][1]
                 last_end = jobs[i][1] + jobs[i][0]
-                print(" heap:", heap, "tot:", tot, "last_end", last_end)
             else:
                     # 위 while문에 의해 갱신된 last_end에 대해서 job의 수행 시작 시간이 더 앞에 있는지
                     # 즉, 바로 직전 작업을 하드 디스크가 하는 도중에 job이 들어왔을 때
+                changeValueInHeap(heap)
                 pushJob(heap, jobs, i)
         else:
             # 하드디스크가 작업을 수행하고 있는 경우
             pushJob(heap, jobs, i)
         i+=1
     while(len(heap)!=0): popJob(heap, jobs)
-    # return tot
     return tot//len(jobs)
 
+def changeValueInHeap(cfrom):
+    global temp, last_end
+    while(len(cfrom)!=0):
+        predt, stt, fend = heapq.heappop(cfrom)
+        temp.append([predt-fend+last_end, stt, last_end])
+    while(len(temp)!=0):
+        predt, stt, fend = temp.popleft()
+        heapq.heappush(cfrom, (predt, stt, fend))
+
+
 def pushJob(heap, jobs, i):
-    global first_le, tot, last_end
+    global tot, last_end
     heapq.heappush(heap, (last_end+jobs[i][1], jobs[i][0], last_end))
-    # first_le = last_end
-    print(" heap:", heap, "tot:", tot, "last_end", last_end)
 
 def popJob(heap, jobs):
     # heap에 있는 애들 다 빼서 tot에 합해 주기(하드디스크가 작업하는 도중에 요청 들어오는 게 끝남)
-    global first_le, tot, last_end
+    global tot, last_end
     predt, stt, fend = heapq.heappop(heap)
     perft = predt - fend; waitt = last_end - stt
     tot += perft + waitt
     last_end += perft
-    print("waitt: ", waitt)
-    print(" heap:", heap, "tot:", tot, "last_end", last_end)
+
+def solution1(jobs):
+    # ! 다른 사람 풀이
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs], key=lambda x: (x[1], x[0])))
+    q = []
+    heapq.heappush(q, tasks.popleft())
+    current_time, total_response_time = 0, 0
+    while len(q) > 0:
+        dur, arr = heapq.heappop(q)
+        current_time = max(current_time + dur, arr + dur)
+        total_response_time += current_time - arr
+        while len(tasks) > 0 and tasks[0][1] <= current_time:
+            heapq.heappush(q, tasks.popleft())
+        if len(tasks) > 0 and len(q) == 0:
+            heapq.heappush(q, tasks.popleft())
+    return total_response_time // len(jobs)
 
 # print(solution([[0, 3], [1, 9], [2, 6]]))
 # print(solution([[0, 100], [75, 50], [96, 10], [160, 30]]))
 # print(solution([[0, 20], [3, 4], [3, 5], [17, 2]]))
-print(solution([[0, 10], [2, 10], [9, 10], [15, 2]]))
+# print(solution([[0, 10], [2, 10], [9, 10], [15, 2]]))
 # print(solution([[0, 10], [2, 12], [9, 19], [15, 17]]))
+
+# print(solution([[0, 3], [1, 9], [2, 6]]))
+# print(solution([[0, 1]]), 1)
+
+# print(solution([[1000, 1000]]), 1000)
+# print(solution([[0, 1], [0, 1], [0, 1]]), 2)
+# print(solution([[0, 1], [0, 1], [0, 1], [0, 1]]), 2)
+# print(solution([[0, 1], [1000, 1000]]), 500)
+# print(solution([[100, 100], [1000, 1000]]), 500)
+# print(solution([[10, 10], [30, 10], [50, 2], [51, 2]]), 6)
+# print(solution([[0, 3], [1, 9], [2, 6], [30, 3]]), 7)
+
+# 여기서부터 적용되는 반례
+# print(solution([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]), 72)
+# print(13, solution([[0, 9], [0, 4], [0, 5], [0, 7], [0, 3]]))
+# print(72, solution([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 2], [15, 34], [35, 43], [26, 1]]))
+# print(72, solution([[24, 10], [28, 39], [43, 20], [37, 5], [47, 22], [20, 47], [15, 34], [15, 2], [35, 43], [26, 1]]))
+# print(13, solution([[1, 9], [1, 4], [1, 5], [1, 7], [1, 3]]))
