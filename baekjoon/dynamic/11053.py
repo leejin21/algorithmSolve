5cf591113314ec0de885aa18292d2cfd1a3cb15d
Jin Lee <leejin21c@gmail.com>
2021-05-25 01:31:55 +0900

    백준: 가장 긴 증가하는 부분 수열 end

--- baekjoon/dynamic/11053.py
+++ baekjoon/dynamic/11053.py
@@ -27,46 +27,40 @@ SOLUTION
 1 1 1 1 1 2 2 2 2 2
 
 
-[진짜 반례]
+[진짜 반례1]
 11
 10 20 10 30 20 21 22 23 24 25 50 
 -> 10 20 21 22 23 24 25 50: 8
 
-
 부분 수열이므로 어떻게던 가능하다.
-
-시드를 박아둬야 함.
-시드의 조건: 직전보다 작아야 함, 등장한 시드들과 비교했을 떄 가장 작은 값.
-
-A_수열 = {1, 2, 1, 3, 2, 4}
-
 재귀 써서 여러 경우로 생각해야 함.
 
+[진짜 반례2]
+11
+10 50 10 50 1 2 3 4 10 50 10
 
 
 '''
 import sys; read = sys.stdin.readline
+sys.setrecursionlimit(10000)
+
+def getLongPartNums(i):
+    if from_behind[i]:
+        return from_behind[i]
+    for j in range(i+1, len(A)):
+        if A[i] == A[j]:
+            continue
+        elif A[i] > A[j]:
+            getLongPartNums(j)      # 새로 시작!
+        elif A[i] < A[j]:
+            from_behind[i] = max(from_behind[i], getLongPartNums(j) + 1)
+    if not from_behind[i]: from_behind[i] = 1
+        # 진짜 반례 2에서 유추해낸 코드
+    return from_behind[i]
+    
 N = int(read())
 A = list(map(int, read()[:-1].split()))
+from_behind = [0]*(len(A)-1)+[1]
 
-parts = []
-
-def getLongPartNums():
-    pass
-
-for i in range(len(A)):
-    # change = False
-    if i == 0:
-        parts.append([A[i], 1])
-        continue
-    for j, p in enumerate(parts):
-        if A[i] > p[0]:
-            parts[j][0] = A[i]; parts[j][1] += 1
-            # change = True
-        # elif A[i] == p[0]:
-        #     change = True
-    parts.append([A[i], 1])
-        
-parts.sort(key=lambda x: x[1], reverse=True)
-print(parts)
-print(parts[0][1])
+getLongPartNums(0)
+print(max(from_behind))
