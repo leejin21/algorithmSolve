3404db57f360a9d327dd510a2ad0950f7da3a6c8
Jin Lee <leejin21c@gmail.com>
2021-06-27 17:06:20 +0900

    백준: 집합 finish

--- baekjoon/hash_map_set/11723.py
+++ baekjoon/hash_map_set/11723.py
@@ -2,28 +2,68 @@ import sys; read=sys.stdin.readline
 
 M = int(read())
 
-S = [0]*21
+ans_list = []
 
-for _ in range(M):
-    cmd = read()[:-1].split(" ")
-    if cmd[0] == "add":
-        S[int(cmd[1])] = 1
-    elif cmd[0] == "check":
-        print(S[int(cmd[1])])
-    elif cmd[0] == "remove":
-        S[int(cmd[1])] = 0
-    elif cmd[0] == "toggle":
-        S[int(cmd[1])] = 0 if S[int(cmd[1])] else 1
-    elif cmd[0] == "all":
-        for i in range(len(S)):
-            S[i] = 1
-        
-    elif cmd[0] == "empty":
-        for i in range(len(S)):
-            S[i] = 0
-        
 
-    
+def main2():
+    S = set()
+    for _ in range(M):
+        cmd_list = read()[:-1].split(" ")
+        cmd = cmd_list[0]
+        if len(cmd_list)>1:
+            i = int(cmd_list[1]) 
+        else: i = None
+        isthereans = False; ans = 0
+        if cmd == "add":
+            S.add(i)
+        elif cmd == "check":
+            isthereans = True
+            if i in S:
+                ans = 1
+            else:
+                ans = 0
+        elif cmd == "remove":
+            S.discard(i)
+        elif cmd == "toggle":
+            if i in S:
+                S.discard(i)
+            else:
+                S.add(i)
+        elif cmd == "all":
+            S = set([i for i in range(1, 21)])
+        elif cmd == "empty":
+            S = set([])
+
+        if isthereans:
+            # ans_list.append(ans)
+            print(ans)
+        # print(S)
+
+main2()
+# print(ans_list)
+
+
+
+def main():
+    S = [0]*21
+    for _ in range(M):
+        cmd = read()[:-1].split(" ")
+        ans = False
+        if cmd[0] == "add":
+            S[int(cmd[1])] = 1
+        elif cmd[0] == "check":
+            ans = S[int(cmd[1])]
+        elif cmd[0] == "remove":
+            S[int(cmd[1])] = 0
+        elif cmd[0] == "toggle":
+            S[int(cmd[1])] = 0 if S[int(cmd[1])] else 1
+        elif cmd[0] == "all":
+            S = [1 for i in range(len(S))]
+        elif cmd[0] == "empty":
+            S = [0 for i in range(len(S))]
+
+        if ans != False:
+            print(ans)
 
 
 '''
