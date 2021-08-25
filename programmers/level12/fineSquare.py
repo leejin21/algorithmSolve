f53b90676205bafd21876b2bf633bf8f5830a3d6
Jin Lee <leejin21c@gmail.com>
2021-08-25 11:48:14 +0900

    프로그래머스: 멀쩡한 사각형 TRY 2(시간초과)

--- programmers/level12/fineSquare.py
+++ programmers/level12/fineSquare.py
@@ -6,16 +6,17 @@ from math import gcd
 
 def solution(w,h):
     answer = 1
-    GCD = gcd(w, h)
+    # GCD = gcd(w, h)
     cnt = 0
     tot_x = max(w, h)
     tot_y = min(w, h)
     if tot_x == tot_y:
         return w*h - w
     # print(tot_x, tot_y, GCD)
-    incli_x = tot_x // GCD
-    incli_y = tot_y // GCD
-
+    # incli_x = tot_x // GCD
+    # incli_y = tot_y // GCD
+    incli_x = tot_x
+    incli_y = tot_y
     incli = incli_y/ incli_x
 
     pre_y = 0
@@ -23,7 +24,11 @@ def solution(w,h):
     for x in range(1, incli_x+1):
         y = x * incli
         # print(x, y, pre_y)
-        if int(y) == int(pre_y) or (y-int(y) == 0 and int(y)-1 == int(pre_y)):
+        if y-int(y) == 0:
+            cnt += 1
+            break
+
+        elif int(y) == int(pre_y):
             # 아직 y를 못 넘겼거나, 마지막(x=incli_x) == y가 정수일 경우
             cnt +=1
             
@@ -33,7 +38,7 @@ def solution(w,h):
             pre_y = y
         # print("cnt", cnt)
     # print(cnt_list)
-    return w*h - cnt*(tot_x//incli_x)
+    return w*h - cnt*(tot_x//x)
 
 
 
