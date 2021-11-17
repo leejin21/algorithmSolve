02904abaa072d9a5332c89ab49b02a12ae77d15c
JIN <leejin21c@sookmyung.ac.kr>
2021-11-17 12:30:09 +0900

    프로그래머스: 카카오 후보키 [TRY 2: 성공]

--- programmers/kakao/42890.py
+++ programmers/kakao/42890.py
@@ -11,7 +11,7 @@
 
 그래서 만약 컬럼 1, 컬럼 2가 유일성 만족하지 않을 경우: 컬럼1-컬럼2-컬럼3으로 확인.
 
-
+<<TRY 1>>
 <반례>
 [["100","ryan","music","2"],
 ["200","apeach","math","2"],
@@ -23,38 +23,47 @@
 tot_accum = [['100', '200', '300', '400', '500', '600'], ['ryan-music-2', 'apeach-math-2', 'tube-computer-3', 'con-computer-4', 'muzi-music-3', 'apeach-math-4'], ['ryan-2', 'apeach-2', 'tube-3', 'con-4', 'muzi-3', 'apeach-4'], ['music-2', 'math-2', 'computer-3', 'computer-4', 'music-3', 'math-4']]
 최소성을 만족시키기 어려움.
 
+<<TRY 2>>
 
 '''
 
-import sys
-import collections
+import collections, itertools
 
-sys.setrecursionlimit(10000)
 def solution(relation):
     answer = [0]
-    tot_accum = []
     pairs = []
-    def recursive(i, accum, accum_pair):
-        # print(i, accum, answer)
-        add = list(map(lambda x:x[i], relation))
-        new_accum = []
-        if len(accum) == 0:
-            new_accum = add
-        else:
-            for j in range(len(accum)):
-                new_accum.append(accum[j]+'-'+add[j])
-        # print(new_accum)
-        
-        if collections.Counter(new_accum).most_common(1)[0][1] <= 1:
-            tot_accum.append(new_accum)
-            answer[0] += 1
-            return
-        else:
-            for k in range(i+1, len(relation[0])):
-                recursive(k, new_accum)
-    for i in range(len(relation[0])):
-        recursive(i, [])
-    print(tot_accum)
+    columns = ''.join([str(i) for i in range(len(relation[0]))])
+    for cnt in range(len(relation[0])):
+        for it in list(itertools.combinations(columns, cnt+1)):
+            EXIST = False
+            for i in range(1, len(it)+1):
+                # i: 조합의 결과쌍의 길이
+                for j in list(itertools.combinations(''.join(it), i)):
+                    # j: 조합
+                    if ''.join(j) in pairs:
+                        EXIST = True
+                        break
+                if EXIST:
+                    break
+
+            if not EXIST:
+                # 해당 컬럼 집합이 후보키가 될 수 있는 지 확인
+                col_idxs = [int(i) for i in it]
+                accum = []
+                for i in col_idxs:
+                    add = list(map(lambda x:x[i], relation))
+                    if len(accum) == 0:
+                        new_accum = add
+                    else:
+                        new_accum = []
+                        for j in range(len(accum)):
+                            new_accum.append(accum[j]+'-'+add[j])
+                    accum = new_accum
+                if collections.Counter(accum).most_common(1)[0][1] <= 1:
+                    answer[0] += 1
+                    pairs += [''.join(i) for i in list(itertools.permutations(''.join(it), len(it)))]
+                    # TODO pair += 어쩌구
+                    
     return answer[0]
 
 # print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
